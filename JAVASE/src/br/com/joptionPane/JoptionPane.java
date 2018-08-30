57f038a78dee8fc2ef1f29ca25925c22dbc8d544
root <root@DESKTOP-6HNAVOF>
2018-08-30 11:15:35 -0300

    method main, out attribute, system class

---
+++ JAVASE/src/br/com/joptionPane/JoptionPane.java
@@ -0,0 +1,14 @@
+package br.com.joptionPane;
+
+import javax.swing.JOptionPane;
+
+public class JoptionPane {
+
+public static void main(String[] args) {
+
+	JOptionPane.showMessageDialog(null,"Bem vindo ao java");
+	System.exit(0);	
+		
+	}
+
+}
---
+++ JAVASE/src/br/com/numeros/literais/NumerosLiterais.java
@@ -0,0 +1,24 @@
+package br.com.numeros.literais;
+
+public class NumerosLiterais {
+
+	public static void main(String[] args) {
+		
+		System.out.println();
+		System.out.println("inteiro decimal:\t\t" + 11);
+		System.out.println("inteiro hexadecimal:\t\t" + 0xb);
+		System.out.println("inteiro octa:\t\t\t" + 013);
+		System.out.println("inteiro longo:\t\t\t" + 11L);
+		System.out.println("real precisão simples:\t\t" + 24.2f);
+		System.out.println("real precisão dupla:\t\t" + 24.2);
+		System.out.println("tipo lógico:\t\t\t" + true);
+		System.out.println("tipo caractere:\t\t\t" + 'h');
+		System.out.println("text:\t\t\t\t" + "ANA");
+		System.out.println();
+
+
+		
+		
+	}
+
+}
