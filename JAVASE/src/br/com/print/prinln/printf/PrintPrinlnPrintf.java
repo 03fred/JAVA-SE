d31f7d0b5f5e0d4460c6b33c29936010091f89da
03fred <fred.rocke02@gmail.com>
2019-08-26 15:22:41 -0300

    bubble sort

--- JAVASE/src/br/com/print/prinln/printf/PrintPrinlnPrintf.java
+++ JAVASE/src/br/com/print/prinln/printf/PrintPrinlnPrintf.java
@@ -4,32 +4,50 @@ public class PrintPrinlnPrintf {
 
 	public static void main(String[] args) {
 
-		// CLASSE SYSTEM
+		/* // CLASSE SYSTEM
 		// ATRIBUTO OUT
-		// MÉTODO PRINT
+		// Mï¿½TODO PRINT
 
 		System.out.print('A');
 		System.out.print("\n");
-		System.out.print("Um número Inteiro:\t");
+		System.out.print("Um nï¿½mero Inteiro:\t");
 		System.out.print(123);
 		System.out.print("\n");
 
-		// MÉTODO PRINTLN
-		System.out.println("Um número decimal:\t" + 4.3);
+		// Mï¿½TODO PRINTLN
+		System.out.println("Um nï¿½mero decimal:\t" + 4.3);
 		System.out.println("Um valor booleano:\t" + true);
 		System.out.println("Uma barra invertida:\t\\");
 		System.out.println("Um texto entre aspas:\t" + "\"Rui\"");
 		System.out.println();
 
 		// SAIDAS FORMATADAS
-		// MÉTODO PRINTF
-		System.out.printf("%s é um %s", "Platão", "Filósofo");
+		// Mï¿½TODO PRINTF
+		System.out.printf("%s ï¿½ um %s", "Platï¿½o", "Filï¿½sofo");
 
 		System.out.printf("Um caractere: %c", 'A');
 		System.out.println();
 		System.out.printf("Um texto: %s", "Java em todos os lugares");
 		System.out.println();
-
+*/
+		
+		String str[] = {"fulano","beautrano","ci"};
+		String aux = "";
+	    int i = 0;
+	    for(i = 0; i<3; i++){
+	        for(int j = 0; j<2; j++){
+	            if(str[j].length() > str[j + 1].length()){
+	                aux = str[j];
+	                str[j] = str[j+1];
+	                str[j+1] = aux;
+	            }
+	        }
+	    }
+	    for(i = 0; i<3; i++){
+	        System.out.println(" "+str[i]);
+	    }
+		
+		
 	}
 
 }
